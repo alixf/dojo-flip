a99e128de70823de9e6c5f08be31244a075fa468
Alix Fumoleau <afumoleau@betomorrow.com>
2020-03-13 10:49:48 +0100

    first commit

---
+++ src/app.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import { useState } from "react";
+import styled from "styled-components";
+
+export const App = () => {
+	const [expanded, setExpanded] = useState(false);
+
+	return (
+		<Container>
+			<Card expanded={expanded} onClick={e => setExpanded(e => !e)}>
+				<div>BeTomorrow</div>
+			</Card>
+		</Container>
+	);
+};
+
+const Container = styled.div`
+	width: 100vw;
+	height: 100vh;
+	background-color: #ccc;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+`;
+
+const Card = styled.div<{ expanded: boolean }>`
+	background: white;
+	margin: 10px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.33);
+	width: ${({ expanded }) => (expanded ? "200px" : "100px")};
+	height: ${({ expanded }) => (expanded ? "150px" : "100px")};
+	transition: all 0.33s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+`;
